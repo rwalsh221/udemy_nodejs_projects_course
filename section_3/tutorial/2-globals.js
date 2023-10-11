0f90923ab88080aa830c832fb733857f0c6c51d5
Richard Walsh <richwalsh2@googlemail.com>
2023-10-11 18:36:50 +0100

    section 3 lesson 17

---
+++ section_3/tutorial/2-globals.js
@@ -0,0 +1,11 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname - path to current directory
+console.log(__dirname);
+// __filename - file name
+console.log(__filename);
+// require - function to use modules (commonJS)
+// module - info about current module(file)
+console.log(module);
+// process - info about enviroment where the program is being executed
+console.log(process.env);
---
+++ section_3/tutorial/app.js
@@ -0,0 +1,12 @@
+// MODULES
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-export');
+require('./7-mind-grenade');
+// console.log(data);
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);
