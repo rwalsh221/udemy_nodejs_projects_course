3a2c559625a5cbb3d5a1efb26d0bf2a7bdbae37d
Richard Walsh <richwalsh2@googlemail.com>
2023-10-20 11:25:00 +0100

    section 4 lesson 82

---
+++ section_4/complete/10-middleware-options.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+// req => middleware => res
+
+// app.use([logger, authorize]);
+
+// app.use(express.static('./public'));
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+app.get('/about', (req, res) => {
+  console.log(req.user);
+  res.send('about');
+});
+
+app.get('/api/products', (req, res) => {
+  res.send('products');
+});
+
+app.get('/api/items', (req, res) => {
+  res.send('items');
+});
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000');
+});
