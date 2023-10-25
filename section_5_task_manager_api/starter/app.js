85768145d5b332424ee33e8fc815f3f56c2fdc09
Richard Walsh <richwalsh2@googlemail.com>
2023-10-25 18:26:45 +0100

    section 6 lesson 108

--- section_5_task_manager_api/starter/app.js
+++ section_5_task_manager_api/starter/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasksRouter = require('./routes/tasksRouter');
+const connectDb = require('./db/connect');
+require('dotenv').config();
 
 const port = 5000;
 
@@ -15,6 +17,15 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasksRouter);
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- section_5_task_manager_api/starter/controllers/tasksController.js
+++ section_5_task_manager_api/starter/controllers/tasksController.js
@@ -1,8 +1,11 @@
+const TaskModel = require('../models/TaskModel');
+
 const getAllTasks = (req, res) => {
   res.send('get all task');
 };
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await TaskModel.create(req.body);
+  res.status(201).json({ task });
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
