1c6d4894b3904eb71957923576d5317107897b4b
Richard Walsh <richwalsh2@googlemail.com>
2023-11-02 11:21:52 +0000

    section 7 lesson 149

--- section_7_store_api/starter/controllers/productsController.js
+++ section_7_store_api/starter/controllers/productsController.js
@@ -1,15 +1,20 @@
 const ProductModel = require('../models/productModel');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await ProductModel.find({}).sort('-name price');
+  const products = await ProductModel.find({ price: { $gt: 100 } })
+    .sort('price')
+    .select('name price')
+    .limit(10)
+    .skip(0);
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
 const getAllProducts = async (req, res) => {
   const querys = ['featured', 'name', 'company'];
-  const { featured, name, company, sort } = req.query;
+  const { sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
+  // QUERYS
   querys.map((query) => {
     if (req.query[query]) {
       if (query === 'name') {
@@ -19,6 +24,31 @@ const getAllProducts = async (req, res) => {
       }
     }
   });
+  // console.log(req.query);
+  if (numericFilters) {
+    console.log(numericFilters);
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    console.log(filters);
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+      console.log(queryObject);
+    });
+  }
 
   // if (featured) {
   //   queryObject.featured = featured;
@@ -28,14 +58,27 @@ const getAllProducts = async (req, res) => {
   // }
 
   let result = ProductModel.find(queryObject);
-  console.log(result);
-  // let products;
+  // console.log(result);
+  // SORT
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result.sort('createdAt');
   }
+
+  // SELECT
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ nbHits: products.length, data: products });
 };
