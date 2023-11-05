e6c8b96841a7a8712d0a8e2460ff4a052b40b3d3
Richard Walsh <richwalsh2@googlemail.com>
2023-11-05 21:53:58 +0000

    section 9 lesson 179

--- section_9_jobs_api/starter/controllers/auth.js
+++ section_9_jobs_api/starter/controllers/auth.js
@@ -1,5 +1,14 @@
+const UserModel = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors/index');
+const jwt = require('jsonwebtoken');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const user = await UserModel.create({ ...req.body });
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
---
+++ section_9_jobs_api/starter/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcryptJs = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide a email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+});
+
+UserSchema.pre('save', async function (next) {
+  const salt = await bcryptJs.genSalt(10);
+  this.password = await bcryptJs.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
